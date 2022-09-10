ead94b0128233a139c6056180e30b904cd5a5789
Itai Fish <chessandpotatoes@gmail.com>
2022-09-10 15:35:20 -0400

    Rewrite page to be easier to add new components in header

---
+++ src/components/organisms/pageWithNav.tsx
@@ -0,0 +1,30 @@
+import { Footer } from '../molecules/footer';
+
+import { NavBar } from '../molecules/navBar';
+
+import NavigableComponentList from '../molecules/wrappers/NavigableComponentList';
+
+interface PageWithNavProps {
+	links: {
+		name: string;
+		scrollTo: string;
+		element: JSX.Element;
+		inNav: boolean;
+	}[];
+}
+
+function PageWithNav(props: PageWithNavProps) {
+	return (
+		<>
+			<NavBar links={props.links.filter((link) => link.inNav)} />
+			<NavigableComponentList
+				components={props.links.map((link) => {
+					return { element: link.element, id: link.scrollTo };
+				})}
+			/>
+			<Footer />
+		</>
+	);
+}
+
+export default PageWithNav;
